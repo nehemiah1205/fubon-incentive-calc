aae4aba6a7e96fa3178655cad029188e22fb71ae
nehemiah1205 <93313163+nehemiah1205@users.noreply.github.com>
2026-08-14 13:05:34 +0800

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// 部署到 GitHub Pages 時，base 要設成你的 repo 名稱，例如：
-// https://<你的帳號>.github.io/<repo名稱>/
-// 若之後改 repo 名稱，記得同步修改這裡
+// 如果您的 GitHub repo 名稱不是 fubon-incentive-calc，
+// 請把下面 base 的值改成 '/您的repo名稱/'
 export default defineConfig({
-  plugins: [react()],
   base: '/fubon-incentive-calc/',
+  plugins: [react()],
+})
